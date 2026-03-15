8e2c35b346ee138ab33e0345067ffd10a214952c
PatrickSt1991 <patrick.stel@kpnmail.nl>
2026-03-15 14:10:46 +0100

    prodution ready

--- src/api/apiConfig.ts
+++ src/api/apiConfig.ts
@@ -1,4 +1,4 @@
-export const API_BASE = import.meta.env.VITE_API_BASE || "http://localhost:5027/api";
+export const API_BASE = import.meta.env.VITE_API_BASE;
 
 export const endpoints = {
     authentication: `${API_BASE}/Auth`,
